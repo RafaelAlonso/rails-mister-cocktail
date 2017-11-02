c65083dff2328176bd3be630a862fc658bfbe275
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-02 16:14:14 -0400

    styled colors and spacing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:create]
+    # collection do
+    #   get '/top', to: "cocktails#top"
+    # end
   end
   delete 'doses/:id', to: 'doses#destroy'
 
