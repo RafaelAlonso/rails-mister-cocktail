03826d5a0938afcc05172a979aad05460ca41adb
Rafael Alonso <rafael.palonso@gmail.com>
2017-11-03 16:55:45 -0400

    Finished styling for today

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -2,5 +2,6 @@ class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
 
+  validates :ingredient, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
